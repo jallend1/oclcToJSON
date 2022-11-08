8062de1272a4c72566c8852b7ff77a8b68634d75
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-11-07 16:24:39 -0800

    Updates app to overwrite existing JSON file instead of appending

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const { createReadStream, createWriteStream, readFileSync } = require('fs');
+const { createReadStream, writeFileSync } = require('fs');
 const { validateFile } = require('./commandLine');
 const { parse } = require('csv-parse');
 
 const extractDataFromCSV = (localDataSource) => {
-  const targetFile = createWriteStream('lendingLibraries.json');
   const borrowingLibraries = [];
   createReadStream(localDataSource)
     .pipe(parse({ delimiter: '\t', from_line: 14, relax_column_count: true }))
     .on('data', (row) => {
-      //   const [fileMonth, fileYear] = dataDateArray;
       const locationInfo = {
         name: row[0],
         institutionSymbol: row[1],
@@ -22,7 +20,14 @@ const extractDataFromCSV = (localDataSource) => {
       borrowingLibraries.push(locationInfo);
     })
     .on('end', () => {
-      targetFile.write(JSON.stringify(borrowingLibraries));
+      writeFileSync(
+        'lendingLibraries.json',
+        JSON.stringify(borrowingLibraries),
+        {
+          encoding: 'utf8',
+          flag: 'w'
+        }
+      );
     });
 };
 
